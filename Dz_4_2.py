a0ac6296c333146d962ba08c8a3403fe005d58c0
@YehorChykin <mebel4268@gmail.com>
2024-01-31 11:24:55 +0100

    Dz_4_2

---
+++ Dz_4_2.py
@@ -0,0 +1,22 @@
+import re
+
+path = r"D:\Projects\start_python\modul_4\modul_4\cats.txt"
+
+def get_cats_info(path):
+    try:
+        with open(path, 'r', encoding='UTF-8') as cats:
+            list_cats=[]
+            read_cats = cats.readlines()
+
+            for cat in read_cats:
+                divide_into_parts= cat.strip().split(',')
+                cat_id, cat_name, cat_age = divide_into_parts
+                cats_info={"id": cat_id, "name": cat_name, "age": cat_age}
+                list_cats.append(cats_info)
+
+    except Exception as e:
+        print(f"Error: {e}")
+    return list_cats
+
+cats_info = get_cats_info(path)
+print(cats_info, sep ='**')
